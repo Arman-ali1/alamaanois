bd676c8ce0c8d5b2b13589c553a3eadc5f6e8328
Arman Ali <armnsrn173200@gmail.com>
2024-10-08 22:30:54 +0530

    Add new files and components for demo purposes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,55 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './components/home/Home.jsx'
-import Service from './components/Services/Service.jsx'
-// import Blankspace from './components/Blankspace/Blankspace.jsx'
-import Clients from './components/Clients/Clients.jsx'
-import Join from './components/Joinprocess/Join.jsx'
-import Testimonial from './components/Testimonial/Testimonial.jsx'
+import { useState } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Courses from "./pages/Courses";
+import Library from "./pages/Library";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Donate from "./pages/Donate";
+import ComingSoon from "./pages/ComingSoon";
+import Footer from "./components/Footer";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const HeaderWrapper = () => {
+    const location = useLocation();
+    return location.pathname !== "/login" ? <Header /> : null;
+  };
+
+  const FooterWrapper = () => {
+    const location = useLocation();
+
+    return location.pathname !== "/login" ? <Footer /> : null;
+  };
 
   return (
     <>
-      <div className='bg-white text-slate-950  w-screen overflow-x-hidden border-b border-gray-500 '>
-      <Home></Home>
-      {/* <Blankspace></Blankspace> */}
-      <Service></Service>
-      <Clients></Clients>
-      <Join></Join>
-      <Testimonial></Testimonial>
-      </div>
+      <Router>
+        <HeaderWrapper />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<ComingSoon />} />
+          <Route path="/courses" element={<ComingSoon />} />
+          <Route path="/library" element={<ComingSoon />} />
+          <Route path="/blogs" element={<ComingSoon />} />
+          <Route path="/contact" element={<ComingSoon />} />
+          <Route path="/login" element={<ComingSoon />} />
+          <Route path="/donate" element={<ComingSoon />} />
+        </Routes>
+        <FooterWrapper />
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div className="maindivabout my-20">
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+        <h1>About</h1>
+      </div>
+    </>
+  );
+};
+
+export default About;
